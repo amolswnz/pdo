eeb1956b037cd988c2735d3888d8fb16ae98446f
Amol Wankhede <amolsw.nz@gmail.com>
2016-12-05 12:21:22 +1300

    New function added
    Added function to generate random password

--- function.php
+++ function.php
@@ -21,3 +21,18 @@ function getInitials($string) {
     return $acronym;
 }
 
+function getRandomPassword($length = 10, $special="!@%#") {
+    $letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUWXYZ";
+    $digits = "1234567890";
+    $password = substr(str_shuffle($letters), 0, $length-4);    // Random letters
+    $password .= substr(str_shuffle($digits), 0, 2);            // At least two numbers
+    $password .= substr(str_shuffle($special), 0, 2);           // At least two special characters
+
+    do {
+        $password = str_shuffle($password);         // Shuffles the characters of generated password
+        if( ! in_array($password[0], str_split($special)) && ! in_array($password[0], str_split($digits)) )
+            return $password;
+        // IF first character of the password is not a special or a digit THEN return password
+        // ELSE loop again 
+    } while(1);
+}

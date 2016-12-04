bc9dd1b65fa93229d6385afe4ef43daca18182a4
Amol Wankhede <amolsw.nz@gmail.com>
2016-12-05 11:29:50 +1300

    PDO charset added

--- connect-inc.php
+++ connect-inc.php
@@ -11,6 +11,7 @@
         $pdo = new PDO("mysql:host=" .HOST . ";dbname=" . DATABASE, USERNAME, PASSWORD);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $pdo->exec("SET NAMES 'utf8'");
         return $pdo;
     } catch(PDOException $e) {
         die("<div class='alert alert-error'>ERROR : " . $e->getMessage() . "</div>");

776fac9b4ee28bf1e65a16dd7c0c70c923cfcd60
Amol Wankhede <amolsw.nz@gmail.com>
2016-12-05 15:34:07 +1300

    minor updates

--- connect-inc.php
+++ connect-inc.php
@@ -8,11 +8,11 @@
         Creating connection
     */
     try {
-        $pdo = new PDO("mysql:host=" .HOST . ";dbname=" . DATABASE, USERNAME, PASSWORD);
+        $pdo = new PDO("mysql:host=" . HOST . ";dbname=" . DATABASE, USERNAME, PASSWORD);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $pdo->exec("SET NAMES 'utf8'");
         return $pdo;
     } catch(PDOException $e) {
-        die("<div class='alert alert-error'>ERROR : " . $e->getMessage() . "</div>");
+        die("<div class='alert alert-danger'>ERROR : " . $e->getMessage() . "</div>");
     }

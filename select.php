910e50c7ec7f1c9b5ccdca2fbde17d8d6a789581
Amol Wankhede <amolsw.nz@gmail.com>
2016-12-14 11:14:29 +1300

    missing *

--- select.php
+++ select.php
@@ -1,7 +1,7 @@
 <?php 
     require_once 'connect-inc.php';
 
-    $sql = "SELECT FROM tableName WHERE id = ?";
+    $sql = "SELECT * FROM tableName WHERE id = ?";
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(1, $id);
     $stmt->execute();

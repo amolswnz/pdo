01ed8ea3081b322f48a6dc7f091681e2f91d8a9b
Amol Wankhede <amolsw.nz@gmail.com>
2017-02-02 15:18:41 +1300

    IN query
    The IN (...) added

--- select.php
+++ select.php
@@ -7,3 +7,13 @@
     $stmt->execute();
 
     $result = $stmt->fetchAll();
+
+    // For selecting data using IN (..) query
+
+    $sql = "SELECT * FROM tableName WHERE id IN( $inQuery )";
+
+    $inQuery = implode(',', array_fill(0, count($idsArray), '?'));
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($idsArray);
+
+    $result = $stmt->fetchAll();
